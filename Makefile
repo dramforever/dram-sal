ba6792441c98b4a8416d8c61c6b22816db39d109
dramforever <catmimiao@me.com>
2017-02-16 15:59:46 +0800

    Fix build system

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-.PHONY: all
-all:
+.PHONY: config
+config:
 	bash scripts/config.sh
 	@echo '[  OK  ] Workspaces are built'
+
+.PHONY: all
+all: config
+	ls build/ | xargs -I '{name}' make -C 'build/{name}'
--- scripts/config.sh
+++ scripts/config.sh
@@ -7,12 +7,14 @@ do
     if [ -d vendor/${name} ]
     then
         cp -r vendor/${name}/ build/
+        extra=`ls vendor/${name} | grep '.cpp$'`
     else
         mkdir -p build/${name}
+        extra=
     fi
 
     cat > build/${name}/Makefile <<END
 ${name}: $(readlink -f src/${name}.cpp)
-	\$(LINK.cpp) $^ \$(LOADLIBES) \$(LDLIBS) -o \$@ \`ls *.cpp\` -I .
+	\$(LINK.cpp) $^ \$(LOADLIBES) \$(LDLIBS) -I . $extra -o \$@
 END
 done
